46f637004239c9004604865ec4f5fa1a6ec450f4
yunah <njengaewanjiku@gmail.com>
2020-03-10 17:19:37 +0300

    learningvirenvridentifiersvariablesstrings

---
+++ Variables.py
@@ -0,0 +1,18 @@
+first_name="Eunice"
+last_name="Njenga"
+favourite_num="4"
+female = "True"
+married="False"
+
+print(first_name)
+print(last_name)
+print(favourite_num)
+print(female)
+print(married)
+
+school="TechCamp"
+print(school[4:8])
+print(school[-4:8])
+print(school[4:])
+print(school[-4:])
+print(school[-5])
